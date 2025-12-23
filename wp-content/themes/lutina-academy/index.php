898a57552108e191a1cfcdc78adca21a75da7be9
sko6455008 <koudai645508@gmail.com>
2025-12-23 13:01:39 +0900

    test

--- wp-content/themes/lutina-academy/index.php
+++ wp-content/themes/lutina-academy/index.php
@@ -8,7 +8,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Lutina Academy | ルティナ占いアカデミー</title>
+    <title>Lutina Academy | ルティナ占いアカデミーa</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
